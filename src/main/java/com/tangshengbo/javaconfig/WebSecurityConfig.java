8459e5664db690cafc1f74168970fad8d53bbf26
Tang <867850520tangbo@sina.com>
2017-06-26 10:59:18 +0800

    code format

--- src/main/java/com/tangshengbo/javaconfig/WebSecurityConfig.java
+++ src/main/java/com/tangshengbo/javaconfig/WebSecurityConfig.java
@@ -44,7 +44,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.inMemoryAuthentication()
                 .withUser("admin").password("admin").roles("ADMIN")
                 .and()
