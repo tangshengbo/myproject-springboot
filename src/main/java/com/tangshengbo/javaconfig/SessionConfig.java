1726a8bb1b439a9eddee198075be7185147dd2cc
Tangshengbo <867850520tangbo@sina.com>
2017-08-31 13:18:39 +0800

    add spring boot  log

--- src/main/java/com/tangshengbo/javaconfig/SessionConfig.java
+++ src/main/java/com/tangshengbo/javaconfig/SessionConfig.java
@@ -7,6 +7,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  * Created by Tang on 2017/6/28.
  */
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 300)
 public class SessionConfig {
 }
