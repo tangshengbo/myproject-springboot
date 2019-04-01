f398872304981cbff7c38098176f9f03c36f91c9
Tangshengbo <867850520tangbo@sina.com>
2019-04-01 17:40:34 +0800

    add async code

---
+++ src/main/java/com/tangshengbo/javaconfig/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.tangshengbo.javaconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Created by Tangshengbo on 2019/4/1
+ */
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(20);
+        taskExecutor.setQueueCapacity(10);
+        return taskExecutor;
+    }
+}
--- src/main/java/com/tangshengbo/javaconfig/SchedulingConfig.java
+++ src/main/java/com/tangshengbo/javaconfig/SchedulingConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.time.FastDateFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -20,6 +21,7 @@ public class SchedulingConfig {
 
     private int count;
 
+    @Async
     @Scheduled(cron = "0 0/1 * * * ?") // 每1分钟执行一次
     public void scheduler() {
         logger.info(FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").format(new Date())
